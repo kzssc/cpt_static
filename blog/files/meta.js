c12f8e86af5998b3853802f2cf9e67dd5059bdf0
Sisheng Chris Zhang <admin@vpchris.com>
2022-01-30 13:28:25 +0100

    20220130

---
+++ blog/files/meta.js
@@ -0,0 +1,74 @@
+(function() {
+	var Realmac = Realmac || {};
+
+	Realmac.meta = {
+		
+		// Set the browser title
+		//
+		// @var String text
+		setTitle: function(text) {
+			return document.title = text;
+		},
+		
+		// Set the content attribute of a meta tag
+		//
+		// @var String name
+		// @var String content
+		setTagContent: function(tag, content){
+			// If the tag being set is title
+			// return the result of setTitle
+			if ( tag === 'title' )
+			{
+				return this.setTitle(content);
+			}
+			
+			// Otherwise try and find the meta tag
+			var tag = this.getTag(tag);
+			
+			// If we have a tag, set the content
+			if ( tag !== false )
+			{
+				return tag.setAttribute('content', content);
+			}
+			
+			return false;
+		},
+		
+		// Find a meta tag
+		//
+		// @var String name
+		getTag: function(name) {
+			var meta = document.querySelectorAll('meta');
+			
+			for ( var i=0; i<meta.length; i++ )
+			{
+				if (meta[i].name == name){
+					return meta[i];
+				}
+			}
+			
+			var tag = document.createElement('meta');
+			tag.name = name;
+			document.getElementsByTagName('head')[0].appendChild(tag);
+			
+			return tag;
+		}
+	};
+ 
+	// Object containing all website meta info
+	var websiteMeta = {"943193e708807e515980adee509c58d6-0.html":"Some papers that we have read so far:\n\n\n1. Barnett, Zach. 2020. “Why You Should Vote to Change the Outcome.” Philosophy and Public Affairs 48(4): 422–","archive-january-2022.html":"Archives for January 2022"};
+ 
+	// pageId must match the key in websiteMeta object
+	var url = window.location.pathname;
+	var pageId = url.substring(url.lastIndexOf('/')+1);
+	if (!pageId || pageId.length == 0){
+		pageId = 'index.html';
+	}
+	pageMeta = websiteMeta[pageId];
+ 
+	// If we have meta for this page
+	if (pageMeta){
+		Realmac.meta.setTagContent('description', pageMeta);
+	}
+ 
+ })();
